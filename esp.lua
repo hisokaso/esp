dfef52e76f24515ca723268f32a0f526f7a276f0
hisokaso <eliasbreitenbach756@gmail.com>
2025-07-24 17:32:37 +0200

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,14 +1,4 @@
-for _, modell in pairs(workspace:GetDescendants()) do
-    local humanoid = modell:FindFirstChild("Humanoid")
-    if humanoid and humanoid:IsA("Humanoid") then
-        local highlight = Instance.new("Highlight")
-        highlight.Adornee = modell
-        highlight.FillColor = Color3.new(1, 0, 0)
-        highlight.OutlineColor = Color3.new(1, 1, 1)
-        highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-        highlight.Parent = modell
-    end
-end
+
 --// Variablen
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
